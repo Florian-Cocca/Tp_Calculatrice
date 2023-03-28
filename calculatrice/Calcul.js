74386314257627f548fd1fbbc683a9c2a596180f
Florian-Cocca <90192318+Florian-Cocca@users.noreply.github.com>
2023-03-28 20:03:04 +0200

    Add files via upload

--- calculatrice/Calcul.js
+++ calculatrice/Calcul.js
@@ -1,30 +1,60 @@
-function addChar(char){
-	document.getElementById("equation").textContent += char;
+class calculatrice{
+	constructor(sym){
+		this.sym = sym;
+	}
+	
+	addChar(sym){
+		document.getElementById("ecran").textContent += sym;
+	}
+
+	rep(){
+		document.getElementById("ecran").textContent += document.getElementById("resultat").textContent;
+	}
+
+	removeChar(){
+		if(document.getElementById("ecran").textContent == ""){
+			document.getElementById("ecran").textContent = document.getElementById("equation").textContent;
+		}
+		else{
+			document.getElementById("ecran").textContent = document.getElementById("ecran").textContent.replace(/.$/, '');
+		}
+	}
+
+	clearEquation(){
+		document.getElementById("ecran").textContent = "";
+	}
+
+	getResult(){
+		this.result = eval(document.getElementById("ecran").textContent);
+		if(this.result % 1 != 0){
+			document.getElementById("resultat").textContent = this.result.toFixed(3);
+		}
+		else{
+			document.getElementById("resultat").textContent = this.result;
+		}
+		document.getElementById("equation").textContent = document.getElementById("ecran").textContent;
+		document.getElementById("ecran").textContent = "";
+	}
 }
 
-function addSymbol(sym){
-	document.getElementById("equation").textContent += (" " + sym + " ");
-}
-
-function rep(){
-	document.getElementById("equation").textContent += document.getElementById("resultat").textContent;
-}
-
-function removeChar(){
-	document.getElementById("equation").textContent = document.getElementById("equation").textContent.replace(/.$/, '');
-}
-
-function clearEquation(){
-	document.getElementById("equation").textContent = "";
-}
-
-function getResult(){
-	result = eval(document.getElementById("equation").textContent);
-	if(result % 1 != 0){
-		document.getElementById("resultat").textContent = result.toFixed(3);
+function quack(){
+	if(duck == 0){
+		document.body.style.backgroundImage = "url('Ducky.jpg')";
+		document.body.style.backgroundSize = "20% 20%";
+		coin.play();
+		duck = 1;
 	}
 	else{
-		document.getElementById("resultat").textContent = result;
+		document.body.style.backgroundImage = 'none';
+		unCoin.play();
+		duck = 0;
 	}
-	document.getElementById("equation").textContent = "";
 }
+
+var calc = new calculatrice()
+
+var coin = new Audio('Quack.mp3');
+
+var unCoin = new Audio('Unquack.mp3');
+
+let duck = 0;
